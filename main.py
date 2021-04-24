a52fe5416e6bda0b73c62d38d584b4201ce31d1e
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-04-24 15:32:33 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is the main file. Don't forget to look at the lists.py file to refer on the words/teams we need to manipulate.
+
+# add what you need to import here:
+#import ....
+
+
+# I have set up a variable that combines all of the teams from the list file.
+sports_teams = [lists.football_teams, lists.baseball_teams, lists.basketball_teams]
+sports_teams = lists.football_teams + lists.baseball_teams + lists.basketball_teams
+
+# Print out all the school lunches on the menu, but substitute bratwurst 
+# wherever you see hot dogs
+# Use list comprehension. Just print the list directly so the output will
+# include the brackets and quotations (['item 1', item 2' ... and so on])
+#Hint: With your list comprehension line, you will need to have if, else, and for in one statement
+
+
+
+
+
+# Manipulate the nested lists of sports teams to print all teams from New York
+# and all teams from Los Angeles.  Just print the lists directly so the output will
+# include the brackets and quotations (['team 1', team 2' ... and so on])
+# Note that the list file will have a combination of teams from different places so we have to specify.
+
+#I will make an outline for New York. Follow my example and try it for Los Angeles.
+CheckForNewYork = ['New York']
+#Do your list comprehenstion code here, remember, it should just be one line.
+#Replace "(something)" with your actual code.
+#Hint: NewYorkList = [(something) for (something) in (something) for (something) in (something) if (something) in (something)]
+print(NewYorkList)
+
