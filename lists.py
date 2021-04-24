bb08cdbbe90521c676e215123ea39998f29a3e2a
kaitlinfrani <60160104+kaitlinfrani@users.noreply.github.com>
2021-04-24 15:20:37 -0700

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# This file contains several lists to be used for the assignment.
+# To use them in another Python file, you must import lists, and each
+# reference you make to one of the variables below must use the prefix "lists".
+# For example, to use school_lunches, your code will use "lists.school_lunches"
+
+# Just like how we had to add import random and import turtle in the previous sessions, now we will need to import this file.
+# add: import lists
+
+
+school_lunches = ["hamburgers", "pizza", "hot dogs", "mac 'n cheese", "tacos", 
+                  "hot dogs", "hamburgers", "sloppy joes", "spaghetti", "hot dogs"]
+
+questions = ["name", "age", "favorite team", "favorite class"]
+answers = ["Freddy", "21", "the Los Angeles Angels", "Python Programming"]
+
+football_teams = ("Arizona Cardinals", "Atlanta Falcons", "Baltimore Ravens", 
+                  "Buffalo Bills", "Carolina Panthers", "Chicago Bears", "Cincinnati Bengals", 
+                  "Cleveland Browns", "Dallas Cowboys", "Denver Broncos", "Detroit Lions", 
+                  "Green Bay Packers", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", 
+                  "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers", "Los Angeles Rams", 
+                  "Miami Dolphins", "Minnesota Vikings", "New England Patriots", "New Orleans Saints", 
+                  "New York Giants", "New York Jets", "Philadelphia Eagles", "Pittsburgh Steelers", 
+                  "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Tennessee Titans", 
+                  "Washington Football Team")
+
+baseball_teams = ("Arizona Diamondbacks", "Atlanta Braves", "Baltimore Orioles", 
+                  "Boston Red Sox", "Chicago White Sox", "Chicago Cubs", "Cincinnati Reds", 
+                  "Cleveland Indians", "Colorado Rockies", "Detroit Tigers", 
+                  "Houston Astros", "Kansas City Royals", "Los Angeles Angels", 
+                  "Los Angeles Dodgers", "Miami Marlins", "Milwaukee Brewers", 
+                  "Minnesota Twins", "New York Yankees", "New York Mets", "Oakland Athletics", 
+                  "Philadelphia Phillies", "Pittsburgh Pirates", "San Diego Padres", 
+                  "San Francisco Giants", "Seattle Mariners", "St. Louis Cardinals", 
+                  "Tampa Bay Rays", "Texas Rangers", "Toronto Blue Jays", "Washington Nationals")
+
+basketball_teams = ("Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", 
+                    "Chicago Bulls", "Cleveland Cavaliers", "Dallas Mavericks", 
+                    "Denver Nuggets", "Detroit Pistons", "Golden State Warriors", 
+                    "Houston Rockets", "Indiana Pacers", "Los Angeles Clippers", 
+                    "Los Angeles Lakers", "Memphis Grizzlies", "Miami Heat", 
+                    "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans", 
+                    "New York Knicks", "Oklahoma City Thunder", "Orlando Magic", 
+                    "Philadelphia 76ers", "Phoenix Suns", "Portland Trail Blazers", 
+                    "Sacramento Kings", "San Antonio Spurs", "Toronto Raptors", 
+                    "Utah Jazz", "Washington Wizards")
